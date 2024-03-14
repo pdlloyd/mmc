e41973fe55eb58b1fc134bdf3ccd55bd7ed333a8
Patrick Lloyd <patricklloyd@lucidmotors.com>
2024-03-14 14:07:39 -0700

    Update project name, toolchain, and dependencies

--- assets/sw.js
+++ assets/sw.js
@@ -1,9 +1,9 @@
-var cacheName = 'egui-template-pwa';
+var cacheName = 'mmc-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './mmc.js',
+  './mmc_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
